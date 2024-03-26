f830f08c917b19abe6fed3c26364444d696657f5
unknown <hugocadet76750@gmail.com>
2024-03-26 12:09:41 +0100

    modif

--- lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,12 +1,13 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
     try {
-      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       return credential.user;
     } catch (e) {
       print("Error occured");
@@ -14,9 +15,13 @@ class FirebaseAuthService {
     return null;
   }
 
-  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
     try {
-      UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      var userToken = await credential.user?.getIdToken(true);
+      print("userToken${userToken}");
       return credential.user;
     } catch (e) {
       print("Error occured");
